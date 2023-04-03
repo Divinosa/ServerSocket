526051d266190e95008e9bcd8bb07e69144b501f
“Danila” <Danilabirulevo@gmail.com>
2023-04-03 23:42:23 +0300

    init

---
+++ src/main/java/server/Main.java
@@ -0,0 +1,11 @@
+package server;
+
+import org.example.Server;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        org.example.Server server = new Server();
+    }
+}
